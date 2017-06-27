26ed484f7d03d38feabe43dda154ac0bfe432635
Monika Sharma <lognsystems@gmail.com>
2017-06-27 17:59:12 +0530

    Created Drama and dramadetail api

---
+++ database/testdata/insert_auditorium.sql
@@ -0,0 +1,9 @@
+
+insert into kalrav.auditoriums(id,auditorium_name,address,latitude,longitude,last_edit)
+values(1, "Aspee","Kandivali",124.55,24.5,'2017-05-17 09:25:07');
+
+insert into auditoriums (id,auditorium_name,address,latitude,longitude,last_edit)
+values (2, "Bhaidas","Kandivali",124.55,24.5,'2017-05-17 09:25:07');
+
+insert into auditoriums (id,auditorium_name,address,latitude,longitude,last_edit)
+values (3, "Prabhodhan","Kandivali",124.55,24.5,'2017-05-17 09:25:07');
